caf6b3544dbce49de3236f153663bc7b5e46bdbe
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2018-08-26 17:29:14 +0700

    Update __init__.py

--- xlotp/xlpy/__init__.py
+++ xlotp/xlpy/__init__.py
@@ -1,10 +1,10 @@
 from .config import Config
 from .client import XL
 
-__copyright__       = 'Copyright 2018 by Anggi Alberto'
-__version__         = '1.0.0'
-__author__          = 'Anggi Alberto'
-__author_email__    = 'albertoanggi@gmail.com'
-__url__             = 'http://github.com/albertoanggi/xl-py'
+__copyright__       = 'Copyright 2018 by Bintang Nur Pradana'
+__version__         = '5.0.0'
+__author__          = 'Bintang Nur Pradana'
+__author_email__    = 'bintangnurpradana54@gmail.com'
+__url__             = 'https://github.com/Bintang73/tembaktembakan.git'
 
 __all__ = ['Config','XL']
